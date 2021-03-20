0249cc945e76bea3017b7b5d74081212234c5c6a
Raoul Lefmann <raoul.lefmann@gmail.com>
2021-03-20 12:14:26 +0100

    Cycle through windows with Meta+Tab instead of Alt+Tab

--- config.h
+++ config.h
@@ -18,8 +18,8 @@ static struct key keys[] = {
     {MOD,      XK_c,   win_center, {0}},
     {MOD,      XK_f,   win_fs,     {0}},
 
-    {Mod1Mask,           XK_Tab, win_next,   {0}},
-    {Mod1Mask|ShiftMask, XK_Tab, win_prev,   {0}},
+    {MOD,           XK_Tab, win_next,   {0}},
+    {MOD|ShiftMask, XK_Tab, win_prev,   {0}},
 
     {MOD, XK_d,      run, {.com = menu}},
     {MOD, XK_w,      run, {.com = colors}},
